1defbacae45723ab9faf76113ee7d7a2e4737995
CARLOS MIGUEL GOMEZ ZAMORA <1055283@horusur.corp>
2023-01-10 13:41:56 -0600

    Ruta Hola añadida

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const { application } = require('express');
 const express = require('express');
 const app = Express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/hola', (req, res) => {
+    res.send('Ruta Hola');
+});
 
 
 app.listen(3000, () => {
